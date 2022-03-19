7c75b18ff35704bffaf7a780c677d7af65fffb99
Andrea <onemanshow2099@gmail.com>
2022-03-19 21:32:13 +0100

    secondo esercizio completato

--- 02-dadi/script.js
+++ 02-dadi/script.js
@@ -3,3 +3,15 @@ console.log(numeroGiocatore)
 
 let numeroComputer = Math.floor(Math.random() * 6) + 1;
 console.log(numeroComputer)
+
+if (numeroGiocatore > numeroComputer) {
+    document.getElementById("risultatoDadi").innerHTML += "<p>Vince il giocatore.<p>"
+}
+
+else if (numeroGiocatore < numeroComputer) {
+    document.getElementById("risultatoDadi").innerHTML += "<p>Vince il computer.<p>"
+}
+
+else {
+    document.getElementById("risultatoDadi").innerHTML += "<p>Il giocatore e il computer sono pari.<p>"
+}
