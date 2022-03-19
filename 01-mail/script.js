cb8d49317ce96c726a6b1bf581d8d5983d3c04eb
Andrea <onemanshow2099@gmail.com>
2022-03-19 11:39:45 +0100

    3 aggiornamento

--- 01-mail/script.js
+++ 01-mail/script.js
@@ -4,24 +4,41 @@ let domanda = prompt("inserisci la tua email");
 
 console.log(domanda)
 
-console.log(email[0])
+// console.log(email[0])
 
-console.log(email[1])
+// console.log(email[1])
 
-console.log(email[2])
+// console.log(email[2])
 
 // devo creare un programma che ad ogni ciclo verifichi se il prompt è uguale a una delle email
+
+// for (var i = 0; i < email.length; i++) {
+//     if (domanda == email[i]) {
+//         let risultato = "vero";
+//         console.log(risultato)
+       
+//     }
+
+//     else {
+//         let risultato = "falso";
+//         console.log(risultato)
+//     }
+
+    
+// } non funziona, proviamo a cambiarlo
 
 for (var i = 0; i < email.length; i++) {
     if (domanda == email[i]) {
         let risultato = "vero";
-       
+        console.log(risultato)
+
     }
 
     else {
         let risultato = "falso";
+        console.log(risultato)
     }
 
-    console.log(risultato)
+
 }
 
