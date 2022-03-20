4a2d730a0457e95a7e44352a2fbd03aa113d6abe
Andrea <onemanshow2099@gmail.com>
2022-03-20 03:58:43 +0100

    02 abbellimento secondo esercizio

--- 02-dadi/script.js
+++ 02-dadi/script.js
@@ -1,17 +1,49 @@
-let numeroGiocatore = Math.floor(Math.random()*6) +1;
-console.log(numeroGiocatore)
+// ANCHOR PRIMA VERSIONE. Questo è la prima versione del codice, che si attiva caricando la pagina.
 
-let numeroComputer = Math.floor(Math.random() * 6) + 1;
-console.log(numeroComputer)
+// let numeroGiocatore = Math.floor(Math.random()*6) +1;
+// console.log(numeroGiocatore)
 
-if (numeroGiocatore > numeroComputer) {
-    document.getElementById("risultatoDadi").innerHTML += "<p>Vince il giocatore.<p>"
-}
+// let numeroComputer = Math.floor(Math.random() * 6) + 1;
+// console.log(numeroComputer)
 
-else if (numeroGiocatore < numeroComputer) {
-    document.getElementById("risultatoDadi").innerHTML += "<p>Vince il computer.<p>"
-}
+// if (numeroGiocatore > numeroComputer) {
+//     document.getElementById("risultatoDadi").innerHTML += "<p>Vince il giocatore.<p>"
+// }
 
-else {
-    document.getElementById("risultatoDadi").innerHTML += "<p>Il giocatore e il computer sono pari.<p>"
-}
+// else if (numeroGiocatore < numeroComputer) {
+//     document.getElementById("risultatoDadi").innerHTML += "<p>Vince il computer.<p>"
+// }
+
+// else {
+//     document.getElementById("risultatoDadi").innerHTML += "<p>Il giocatore e il computer sono pari.<p>"
+// }
+
+// ANCHOR SECONDA VERSIONE: questa attiva la simulazione con il click
+
+let bottone = document.getElementById("bottone") ;
+
+bottone.addEventListener(
+    "click",
+    function() {
+        let numeroGiocatore = Math.floor(Math.random() * 6) + 1;
+        console.log(numeroGiocatore)
+
+        let numeroComputer = Math.floor(Math.random() * 6) + 1;
+        console.log(numeroComputer)
+
+        if (numeroGiocatore > numeroComputer) {
+            document.getElementById("risultatoDadi").innerHTML = "<p>Vince il giocatore.<p>"
+        }
+
+        else if (numeroGiocatore < numeroComputer) {
+            document.getElementById("risultatoDadi").innerHTML = "<p>Vince il computer.<p>"
+        }
+
+        else {
+            document.getElementById("risultatoDadi").innerHTML = "<p>Il giocatore e il computer sono pari.<p>"
+        }
+    }
+)
+
+
+
